16ed11929ec0c69bd004bfff5b20c321d19b3226
Mailandr <splintersfinest@gmail.com>
2022-10-10 20:50:45 +0200

    hackerRank Aufgaben

---
+++ hackerRank/src/main/java/hackerRank/einfacheAufgaben/JavaSubstring.java
@@ -0,0 +1,17 @@
+package hackerRank.einfacheAufgaben;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+
+
+
+        String S = "Helloworld";
+        int start = 3;
+        int end = 7;
+
+        System.out.println(S.substring(start,end));
+        //Ergebnis = "lowo"
+
+    }
+}
