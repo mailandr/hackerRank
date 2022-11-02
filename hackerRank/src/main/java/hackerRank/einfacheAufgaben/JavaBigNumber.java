7c9b94c9f90ed59b2aafdec061e46f7801a242fa
Mailandr <splintersfinest@gmail.com>
2022-11-02 19:32:51 +0100

    HackerRank Aufgaben

--- hackerRank/src/main/java/hackerRank/einfacheAufgaben/JavaBigNumber.java
+++ hackerRank/src/main/java/hackerRank/einfacheAufgaben/JavaBigNumber.java
@@ -3,7 +3,7 @@ package hackerRank.einfacheAufgaben;
 import java.util.Scanner;
 import java.math.BigInteger;
 
-public class BigNumber {
+public class JavaBigNumber {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
