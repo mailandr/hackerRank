70354688f34faa02d26cae74231c2eb0c27359a1
Mailandr <splintersfinest@gmail.com>
2022-11-15 22:21:47 +0100

    leet Aufgabe gelöst

--- myWork/src/main/java/leetCode/solved/Problem0136.java
+++ myWork/src/main/java/leetCode/solved/Problem0136.java
@@ -1,4 +1,4 @@
-package leetCode.notSolved;
+package leetCode.solved;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,7 +6,7 @@ import java.util.List;
 public class Problem0136 {
     public static void main(String[] args) {
         //int[] nums = {2, 2, 1};
-        int[] nums = {9, 7, 5, 5, 7};
+        int[] nums = {9, 7, 5, 5, 7, 3, 1, 1, 3, 15, 88, 88, 15};
         //int[] nums = {4, 3, 4, 1, 1};
         //int[] nums = {4, 1, 2, 1, 2};
         //int[] nums = {9};
@@ -17,27 +17,27 @@ public class Problem0136 {
     }
 
     public static int singleNumber(int[] nums) {
-//        if (nums.length == 1) {
-//            return nums[0];
-//        }
-//        List<Integer> list = new ArrayList<>();
-//        List<Integer> listRemove = new ArrayList<>();
-//        for (int num : nums) {
-//            if (!list.contains(num)) {
-//                list.add(num);
-//            } else {
-//                listRemove.add(num);
-//            }
-//        }
-//        list.removeAll(listRemove);
-//
-//        return list.get(0);
-        int num = 0;
-
-        for (int i : nums) {
-            num = num ^ i;
+        if (nums.length == 1) {
+            return nums[0];
         }
-        return num;
+        List<Integer> list = new ArrayList<>();
+        List<Integer> listRemove = new ArrayList<>();
+        for (int num : nums) {
+            if (!list.contains(num)) {
+                list.add(num);
+            } else {
+                listRemove.add(num);
+            }
+        }
+        list.removeAll(listRemove);
+
+        return list.get(0);
+//        int num = 0;
+//
+//        for (int i : nums) {
+//            num = num ^ i;
+//        }
+//        return num;
 
     }
 }
