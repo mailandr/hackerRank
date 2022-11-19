14bd396ff4faa036c3e0d537e1f89bb72ec02c8f
Mailandr <splintersfinest@gmail.com>
2022-11-19 12:10:21 +0100

    Leet Aufgabe erstellt

---
+++ myWork/src/main/java/leetCode/notSolved/Problem0118.java
@@ -0,0 +1,23 @@
+package leetCode.notSolved;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem0118 {
+
+    public static void main(String[] args) {
+//
+//        int numRows = 5;
+//
+//        generate(numRows);
+//    }
+//
+//    public static void generate(int numRows) {
+//        List<List<Integer>> list = new ArrayList<>();
+//
+//        for (int i = 0; i < numRows; i++) {
+//        }
+//
+//    }
+    }
+}
